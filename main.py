cb1f9f9142f1fbad63d793600194ee3800e27453
Yaw Mireku <ymir8091@gmail.com>
2024-06-05 22:39:49 -0700

    Handle form data and make prediction with model

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# import your_ml_model  # Replace with your machine learning model import
 from flask import Flask, render_template, request
 import numpy as np
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
-# Load your pre-trained machine learning model here
+# Load your pre-trained machine learning model
 model = load_model('tiny_200.h5')
 
 @app.route("/")
@@ -15,25 +14,36 @@ def index():
 @app.route("/predict", methods=["POST"])
 def predict():
     if request.method == "POST":
-        # Get user input from the form
-        input_data = request.form.get("input")
+        try:
+            # Get user input from the form
+            time = float(request.form.get("time"))
+            cloudcover = float(request.form.get("cloudcover"))
+            dew = float(request.form.get("dew"))
+            humidity = float(request.form.get("humidity"))
+            precip = float(request.form.get("precip"))
+            precipprob = float(request.form.get("precipprob"))
+            solarenergy = float(request.form.get("solarenergy"))
+            solarradiation = float(request.form.get("solarradiation"))
+            sunelevation = float(request.form.get("sunelevation"))
+            temp = float(request.form.get("temp"))
+            uvindex = float(request.form.get("uvindex"))
 
-        # Preprocess the input data for your model (if needed)
-        # ... your data preprocessing code here ...
+            # Create an array with the input data
+            input_data = np.array([[time, cloudcover, dew, humidity, precip, precipprob,
+                                    solarenergy, solarradiation, sunelevation, temp, uvindex]])
 
-        # Make prediction using your model
-        prediction = input_data # model.predict([preprocessed_data])  # Assuming a list input
+            # Make prediction using your model
+            prediction = model.predict(input_data)
 
-        # Format the prediction for display
-        predicted_class = prediction # prediction[0]  # Assuming single class output
+            # Format the prediction for display
+            predicted_whProduced = prediction[0][0]
 
-        return render_template("result.html", prediction=predicted_class)
+            return render_template("result.html", prediction=predicted_whProduced)
+        except Exception as e:
+            return f"An error occurred: {e}"
 
     else:
         return "Something went wrong. Please try again."
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# if __name__ == '__main__':
-#   app.run(host='0.0.0.0', port=5000)
